36e5172b508bbb78bd6b36963bf4a3eabac32442
KrzysztofNoga <krzysztofnoga1337@gmail.com>
2018-05-27 20:27:18 +0200

    add navbar + footer

--- Script.js
+++ Script.js
@@ -1,20 +1,20 @@
-var CreateTable = function(groupNubmer){
-    $.getJSON(groupNubmer+'.json', function (obj) {
+var CreateTable = function(groupNubmer,week){
+    $.getJSON(groupNubmer+'_'+week+'.json', function (obj) {
+        document.getElementById("topbar").innerHTML += "<div>Grupa: "+groupNubmer+" tydzień: "+week+"</div>";
         for(i=0;i<obj.length;i++)          
         {
             var numberOfDay=i+1;
-            var divId = "d"+numberOfDay;   
-            document.getElementById(divId).innerHTML += '<div style="background-color:  rgb(83, 116, 206)"><center>'+obj[i].day+'</center></div>'; 
-            document.getElementById(divId).innerHTML += '<div style="background-color: rgb(13, 133, 19)"> START: '+obj[i].classes[0].start+"</div></br>";
-            
+            var divId = "d"+numberOfDay;
+              
+            document.getElementById(divId).innerHTML += '<div class="day">'+obj[i].day+'</div>'; 
             for(j=0;j<obj[i].classes.length;j++)
             {
-            document.getElementById(divId).innerHTML += "<b>"+obj[i].classes[j].name+"</b></br>"+obj[i].classes[j].start+" - "+obj[i].classes[j].stop;	 
+            document.getElementById(divId).innerHTML += "<b>"+obj[i].classes[j].name+"</b></br>"+obj[i].classes[j].teacher+"</br>"+obj[i].classes[j].start+" - "+obj[i].classes[j].stop+"</br>"+obj[i].classes[j].class;	 
             if(j<obj[i].classes.length-1)
-            document.getElementById(divId).innerHTML += '<div style="background-color: rgb(13, 133, 19)"> PRZERWA: '+obj[i].classes[j].stop+" - "+obj[i].classes[j+1].start +"</div></br>";
-            else
-            document.getElementById(divId).innerHTML += '<div style="background-color: rgb(13, 133, 19)"> STOP: '+obj[i].classes[obj[i].classes.length-1].stop+"</div>";
+            document.getElementById(divId).innerHTML += '<div class="break"> PRZERWA: '+obj[i].classes[j].stop+" - "+obj[i].classes[j+1].start +"</div>";
+            
+            
         }}    
     });
 }
-CreateTable("L3");
+CreateTable("L3","A");  
